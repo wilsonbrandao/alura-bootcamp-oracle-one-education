6dd22db32dbfae659175333da512cbf82483d5ad
Wilson Brandão <wilsonbrandaocosta@gmail.com>
2022-04-24 19:47:40 -0300

    Conclusao curso html5 e css3 iniciante!

--- Challenges/challenge-1/script.js
+++ Challenges/challenge-1/script.js
@@ -1,5 +1,8 @@
 var input = document.querySelector("input");
 var aside = document.querySelector("aside");
+var paragraph = document.getElementById("paragraph");
+var copySet = true;
+
 var StringTransformedText = '';
 var divAside = document.getElementById("aside-standard");
 input.focus();
@@ -7,8 +10,7 @@ input.focus();
 function Criptografar () {
     var arrayText = input.value.split("");
     var ArrayTextTransformed = [];
-    var paragrath = document.createElement("p");
-    paragrath.classList.add("paragraph-encrypted");
+    buttonCopy.style.display = "flex";
     
     arrayText.forEach(element => {
         if (element == "a"){
@@ -25,17 +27,63 @@ function Criptografar () {
             ArrayTextTransformed.push(element);
         }
     });
-    StringTransformedText = ArrayTextTransformed.join("");
-    divAside.style.display = "none"
-    aside.appendChild(paragrath);
-    paragrath.appendChild(document.createTextNode(StringTransformedText));
-}
-/*
-document.onclick = () =>  {
-    if(input.value == "") divAside.style.display = "block";
-}
-*/
 
+    StringTransformedText = ArrayTextTransformed.join("");
+    divAside.style.display = "none";
+    paragraph.textContent = StringTransformedText;
+    paragraph.style.display = "block";
+    copySet = true;
+
+}
+
+function Descriptografar () {
+    var StringEncrypted = input.value;
+    var StringDecrypted = "";
+
+    
+    StringDecrypted = StringEncrypted.replace(/ai/g, "a");
+    StringEncrypted = StringDecrypted;
+    
+    StringDecrypted = StringEncrypted.replace(/enter/g, "e");
+    StringEncrypted = StringDecrypted;
+    
+    StringDecrypted = StringEncrypted.replace(/imes/g, "i");
+    StringEncrypted = StringDecrypted;
+    
+    StringDecrypted = StringEncrypted.replace(/ober/g, "o");
+    StringEncrypted = StringDecrypted;
+    
+    StringDecrypted = StringEncrypted.replace(/ufat/g, "u");
+    StringEncrypted = StringDecrypted;
+  
+    divAside.style.display = "none";
+    paragraph.textContent = StringDecrypted;
+    paragraph.style.display = "block";
+    copySet = false;
+}
+
+input.onclick = () => {
+    if (divAside.style.display == "none"){
+        divAside.style.display = "block";
+        paragraph.textContent = "";
+        paragraph.style.display = "none";
+    }
+}
 
 var buttonEncrypt = document.getElementById("button-encrypt");
-    buttonEncrypt.onclick = Criptografar;
+buttonEncrypt.onclick = Criptografar;
+
+var buttonDecrypt = document.getElementById("button-decrypt");
+buttonDecrypt.onclick = Descriptografar;
+
+var buttonCopy = document.getElementById("button-copy");
+
+
+function Copy (){
+    if(copySet){
+        navigator.clipboard.writeText(StringTransformedText);
+    }else{
+        navigator.clipboard.writeText(StringDecrypted);
+    }
+}
+buttonCopy.onclick = Copy;
